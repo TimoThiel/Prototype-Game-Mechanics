3adca0b5adf9005d049829570af2de39f3341c31
TimoThiel <91604018+TimoThiel@users.noreply.github.com>
2022-12-17 18:52:24 +0100

    Feature van stopboard toegevoegd

--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -7,6 +7,7 @@ public class Waypoint : MonoBehaviour
     [SerializeField] private float waypointSize = 1f;
     [SerializeField] private GameObject rotondePrefab;
     [SerializeField] private GameObject kruizingPrefab;
+    [SerializeField] private GameObject stopPrefab;
     [SerializeField] private GameObject floatingTextPrefab;
     private GameObject floatingText;
     Transform closestTransform;
@@ -15,6 +16,8 @@ public class Waypoint : MonoBehaviour
     public int minRotonde = 0;
     public int maxKruizing = 0;
     public int minKruizing = 0;
+    public int minStop = 0;
+    public int maxStop = 0;
 
     private void Start()
     {
@@ -23,7 +26,34 @@ public class Waypoint : MonoBehaviour
     }
     private void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0) && maxStop > minStop)
+        {
+            minStop += 1;
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 2.0f;       // we want 2m away from the camera position
+            Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
+            closestTransform = transform.GetChild(0);
+            int index = 0;
+            for (int i = 1; i < transform.childCount; i++)
+            {
+
+                if (Vector3.Distance(closestTransform.position, objectPos) > Vector3.Distance(objectPos, transform.GetChild(i).position) && transform.GetChild(i).CompareTag("Waypoint"))
+                {
+                    index = i;
+                    closestTransform = transform.GetChild(i);
+                }
+            }
+            GameObject stopBoard = Instantiate(stopPrefab, objectPos, Quaternion.identity, transform);
+
+            stopBoard.transform.SetSiblingIndex(index);
+            for (int i = 0; i < stopBoard.transform.childCount; i++)
+            {
+                GameObject rotondeChild = stopBoard.transform.GetChild(i).gameObject;
+                rotondeChild.transform.parent = transform;
+                rotondeChild.transform.SetSiblingIndex(index);
+            }
+            Destroy(stopBoard.transform.gameObject);
+        }
         if (Input.GetMouseButtonDown(0) && maxRotonde>minRotonde)
         {
             minRotonde += 1;
@@ -96,6 +126,14 @@ public class Waypoint : MonoBehaviour
         {
             gameManage.money -= 15;
             maxKruizing += 1;
+        }
+    }
+    public void BuyStopBoard()
+    {
+        if(gameManage.money >= 6)
+        {
+            gameManage.money -= 6;
+            maxStop += 1;
         }
     }
     private void OnDrawGizmos()
